cc5cc40f844d759a34254e641b62278c0a4e6d5c
David Mañanes <dmananes@vexiza.com>
2016-02-27 13:41:29 +0100

    add comment icon

--- app/pages/ecoacher/ecoacher.js
+++ app/pages/ecoacher/ecoacher.js
@@ -19,6 +19,7 @@ export class ECoacherPage {
     {
     	console.log(commentJson);
         this.comment = commentJson;
+        this.comment.iconSrc = 'img/yellow/' + this.comment.icon + '.png';
     };
 
     this.getNewComment = function()
