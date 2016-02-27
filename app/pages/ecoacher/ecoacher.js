75ad5d557583811dc754cc009aa2b0d533120b0e
David Mañanes <dmananes@vexiza.com>
2016-02-27 12:59:20 +0100

    Comment variable

--- app/pages/ecoacher/ecoacher.js
+++ app/pages/ecoacher/ecoacher.js
@@ -1,7 +1,36 @@
 import {Page} from 'ionic-framework/ionic';
+import {Http, Headers, RequestOptions} from 'angular2/http';
 
 
 @Page({
   templateUrl: 'build/pages/ecoacher/ecoacher.html'
 })
-export class ECoacherPage {}
+export class ECoacherPage {
+  static get parameters() {
+    return [[Http]];
+  }
+
+  constructor(http) {
+    this.http = http;
+
+    this.comment = {};
+
+    this.commentReadComplete = function(commentJson)
+    {
+        this.comment = commentJson;
+    };
+
+    this.getNewComment = function()
+    {
+        var readItem = null;
+        this.http.get('http://hackforgood.sockhost.net:3000/getNewComment').map(res=>res.json())
+        .subscribe(
+            data => readItem = data,
+            err => this.logError(err),
+            () => this.commentReadComplete(readItem)
+        );
+    };
+
+  }
+
+}
